9d46e7d62279dc8396414634e030f8a769c383df
thang <thangcom565@gmail.com>
2024-10-01 11:31:19 +0700

    skibidi

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Deptrai");
+        System.out.println("Version 1");
+        System.out.println("day la thang viet");
     }
 }
